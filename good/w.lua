3bafaad3d54c09f4aab49ad73fa5766fc21f43b1
itsagoodtime <yougotwhat@yeah.net>
2025-02-12 16:51:34 +0800

    Add files via upload

---
+++ good/w.lua
@@ -0,0 +1,18 @@
+return {
+    ['MOHUN-Trial'] = {
+        ['yes'] = {
+            ['yess'] = {},
+            ['TIME'] = '',
+        },
+        ['BLACKLIST'] = 'false',
+        ['QQ'] = 'Trial'
+    },
+    ['MOHUN-rp9l4eg8OEY9X2oX'] = {
+        ['NzkzNjg2NjQ4OA=='] = {
+            ['Y2xlYW54bGl0'] = {},
+            ['TIME'] = ''
+        },
+        ['BLACKLIST'] = 'false',
+        ['QQ'] = '3872726683'
+    }
+}
