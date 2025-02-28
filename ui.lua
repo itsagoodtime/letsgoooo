1e36cb1d894f26bce2c8ba2ccd5572fdb4e25a3a
itsagoodtime <yougotwhat@yeah.net>
2025-02-28 22:14:32 +0800

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -2,11 +2,6 @@ if debugX then
 	warn('Initialising Rayfield')
 end
 
-local function getService(name)
-    local service = getService(name)
-    return if cloneref then cloneref(service) else service
-end
-
 local requestsDisabled = getgenv and getgenv().DISABLE_RAYFIELD_REQUESTS
 local InterfaceBuild = '3K3W'
 local Release = tostring(getgenv().moversion) or '未知'
@@ -26,8 +21,8 @@ local settingsTable = {
 	}
 }
 
-local HttpService = getService("HttpService")
-local RunService = getService("RunService")
+local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
 
 -- Environment Check
 local useStudio = RunService:IsStudio() or false
@@ -520,10 +515,10 @@ local RayfieldLibrary = {
 
 
 -- Services
-local UserInputService = getService("UserInputService")
-local TweenService = getService("TweenService")
-local Players = getService("Players")
-local CoreGui = getService("CoreGui")
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local Players = game:GetService("Players")
+local CoreGui = game:GetService("CoreGui")
 
 -- Interface Management
 
@@ -710,7 +705,7 @@ local function makeDraggable(object, dragObject, enableTaptic, tapticOffset)
 	local offset = Vector2.zero
 	local screenGui = object:FindFirstAncestorWhichIsA("ScreenGui")
 	if screenGui and screenGui.IgnoreGuiInset then
-		offset += getService('GuiService'):GetGuiInset()
+		offset += game:GetService('GuiService'):GetGuiInset()
 	end
 
 	local function connectFunctions()
@@ -967,7 +962,7 @@ function RayfieldLibrary:Notify(data) -- action e.g open messages
 		newNotification:Destroy()
 	end)
 end
-Main.Search.Input.PlaceholderText = '搜寻这个界面'
+
 local function openSearch()
 	searchOpen = true
 
